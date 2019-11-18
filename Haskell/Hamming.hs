58c72a33a1ccb8f0370c6618da99682609e5818f
Yao Fang <flavioy@live.com>
2019-11-18 21:12:39 +0000

    Hamming number

---
+++ Haskell/Hamming.hs
@@ -0,0 +1,9 @@
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge xxs@(x : xs) yys@(y : ys) | x == y = x : merge xs ys
+                                   | x < y  = x : merge xs yys
+                                   | x > y  = y : merge xxs ys
+
+ham :: [Integer]
+ham = 1 : merge (map (*2) ham) (merge (map (*3) ham) (map (*5) ham))
