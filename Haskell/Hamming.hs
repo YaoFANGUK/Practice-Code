338847db094493b7a94e261e151846f3d504c5b8
Yao Fang <flavioy@live.com>
2019-11-18 21:20:51 +0000

    汉明数

--- Haskell/Hamming.hs
+++ Haskell/Hamming.hs
@@ -4,5 +4,6 @@ merge [] ys = ys
 merge xxs@(x : xs) yys@(y : ys) | x == y = x : merge xs ys
                                 | x < y  = x : merge xs yys
                                 | x > y  = y : merge xxs ys
+
 ham :: [Integer]
 ham = 1 : merge (map (*2) ham) (merge (map (*3) ham) (map (*5) ham))
