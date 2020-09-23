ff10d898e706198674e01c55a7bf693d768098d8
Fang Yao <flavioy@live.com>
2020-09-23 18:15:57 +0800

    add gitignore file

---
+++ Python/socket/httpServer.py
@@ -0,0 +1,47 @@
+"""
+This document implements a http server
+need:
+- 实现代码，模拟实现服务器，获取通过浏览器客户端访问服务器的请求信息
+- 并将请求报文进行分行显示
+"""
+
+import socket
+import threading
+
+
+class HttpServer:
+    def __init__(self, port):
+        # 1. create a http socket server
+        self.http_server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        # 2. set port reusability
+        self.http_server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEPORT)
+        # 3. bind a port
+        self.http_server_socket.bind(('',port))
+        # 4. set into listen mode
+        self.http_server_socket.listen(128)
+
+    # 5. create a request handler
+    @staticmethod
+    def request_handler(new_socket, port):
+        print(f'a request from {port} has been received')
+        recv_data = new_socket.recv(4096)
+        if len(recv_data) == 0:
+            print('The clinet has colsed the connection')
+            new_socket.close()
+            return
+        recv_str = recv_data.decode('utf-8')
+        for _ in recv_str.splitlines():
+            print(_)
+
+        new_socket.close()
+
+    def start(self):
+        while True:
+            # 4. waiting for a connection with client
+            new_socket, ip_port = self.http_server_socket.accept()
+            # 6. create sub thread to handle request
+            threading.Thread(target=self.request_handler, args=(new_socket, ip_port),daemon=True).start()
+
+
+if __name__ == '__main__':
+    HttpServer(9099).start()
--- Python/代码实现/day04-静态web服务器/02-静态web服务器-返回指定页面.py
+++ Python/代码实现/day04-静态web服务器/02-静态web服务器-返回指定页面.py
@@ -4,39 +4,53 @@
 # 4.判断请求的文件在服务端不存在，组装404状态的响应报文，发送给浏览器
 
 
-
+import socket
 
 if __name__ == '__main__':
     # 1. tcp服务器端socket初始化
-  
-
-
+    tcp_server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    tcp_server_socket.bind(('',9099))
+    tcp_server_socket.listen(128)
 
     # 1.2. 循环等待客户端连接
-
+    while True:
+        new_socket, ip_port = tcp_server_socket.accept()
+        print(f'{ip_port} has connected')
 
         # 2. 接收到浏览器发送的http请求报文
-
+        recv_data = new_socket.recv(1024)
         # 2.1. 判断浏览器关闭（客户端下线）
-
-
+        if len(recv_data) == 0:
+            print('Client has closed the connection')
+            new_socket.close()
             # 跳过当前这次循环
-
-
-
+            continue
+        recv_str = recv_data.decode()
         # 2.2. 提取请求资源路径
         #   按照空格分割请求报文 (GET /index.html HTTP/1.1\r\n ....)
         #   [0]: GET
         #   [1]: /index.html
         #   [2]: ....
         #   split(分割字符， maxsplit=最大分割次数)
-
+        request_path = recv_str.split('', maxsplit=2)[1]
 
         # 2.3. 如果没有请求指定页面，那么设置默认首页
-
-
-        # 3. 读取指定页面的数据，构建响应报文并返回
-
+        try:
+            with open('static' + request_path, 'rb') as f:
+                file_data = f.read()
+        except Exception as e:
+            response_line = 'HTTP/1.1 404 Not Found\r\n'
+            with open('static/' + 'errror.html', 'rb') as f:
+                file_data = f.read()
+        else:
+            response_line = 'HTTP/1.1 200 OK\r\n'
+        finally:
+            # 3. 读取指定页面的数据，构建响应报文并返回
+            response_header = 'Server: PWS1.0\r\n'
+            response_body = file_data
+            response_data = (response_line + response_header + '\r\n').encode('UTF-8') + response_body
+            new_socket.send(response_data)
+            new_socket.close()
         #   3.1 读取指定页面的数据
         #       无论什么请求，我都返回一个指定页面 static + 请求资源路径
         #       "r":读出字符串数据  "rb":读出二进制数据 (支持读任意文件)
